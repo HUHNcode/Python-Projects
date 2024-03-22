4d1628e311b7047b14eda8fa96ea348f396255b3
HUHNcode <164535932+HUHNcode@users.noreply.github.com>
2024-03-22 17:13:42 +0100

    Add files via upload

---
+++ Tutorial_002.py
@@ -0,0 +1,11 @@
+name = "timon"
+age = "13"
+city = "Erfurt"
+animal = "Vögel"
+
+print("mein name ist "+ name)
+print("ich bin " + age + " jahre alt")
+print("ich lebe in " + city + " und das mein leben lang")
+print("ich mag " + animal + "wiel die süß sind")
+
+input('')
